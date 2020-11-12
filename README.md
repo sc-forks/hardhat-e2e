6363eb5dc6b1d5f5349063bc2e937ea1baaef8ec
cgewecke <christophergewecke@gmail.com>
2020-11-12 10:18:36 -0800

    Initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# hardhat-e2e
+A simple Hardhat project to check solidity-coverage installation with in CI
---
+++ hardhat.config.js
@@ -0,0 +1,6 @@
+require("@nomiclabs/hardhat-truffle5");
+require("solidity-coverage");
+
+module.exports = {
+  solidity: { version: "0.5.5" },
+};
