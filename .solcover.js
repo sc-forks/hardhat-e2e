6f0a17f4edb820f027c9c1b3fb71e522ff3de370
cgewecke <christophergewecke@gmail.com>
2021-02-18 13:01:15 -0800

    xx

--- .solcover.js
+++ .solcover.js
@@ -1,9 +1,8 @@
 let istanbulFolder
 
 if (process.env.CI){
-  istanbulFolder = `/tmp/workspace/cov_${process.env.CIRCLE_NODE_INDEX}`
+  //istanbulFolder = `/tmp/workspace/cov_${process.env.CIRCLE_NODE_INDEX}`
 }
 
 module.exports = {
-  istanbulFolder
 }
