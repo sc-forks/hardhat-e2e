b46c08ea25bab24b8a574c9495830cf6525e0f8d
cgewecke <christophergewecke@gmail.com>
2021-02-18 20:50:43 -0800

    junit

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require("solidity-coverage");
 
 module.exports = {
   solidity: { version: "0.5.5" },
+  mocha: {
+    reporter: "mocha-multi-reporters",
+    reporterOptions: {
+      reporterEnabled: "spec, mocha-junit-reporter",
+    }
+  }
 };
